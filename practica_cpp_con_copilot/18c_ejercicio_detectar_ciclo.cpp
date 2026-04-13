ae06c8920d8bbc382806c4b4514214621a481ef0
Daniel Alcantar Noriega <lakiuuen@gmail.com>
2026-04-12 21:00:06 -0700

    12/04/26 - 2

--- practica_cpp_con_copilot/18c_ejercicio_detectar_ciclo.cpp
+++ practica_cpp_con_copilot/18c_ejercicio_detectar_ciclo.cpp
@@ -19,3 +19,90 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+
+struct node {
+    int n;
+    node* nextNode;
+    node(int nW) : n(nW), nextNode(nullptr){};
+    ~node() {
+        std::cout << "node destroyed" << std::endl;
+    }
+};
+node* insertLast(node* nodeInput, int n) {
+            node* newNode = new node(n);
+            if (nodeInput == nullptr) {
+                nodeInput = newNode;
+                return nodeInput;
+            }
+            node* temp = nodeInput; // cause im using pointers; temp and nodeinput has the same pointer in memory
+            while (temp->nextNode != nullptr) {
+                temp = temp->nextNode; // not using nodeInput, cause it will lost the first element pointer
+            }
+            temp->nextNode = newNode;
+            return nodeInput;
+        }
+        bool search(node* &nodeInput, const int& n) {
+            if (nodeInput == nullptr) {
+                return false;
+            }
+            node* temp = nodeInput;
+            while(temp != nullptr) {
+                if (temp->n == n) {
+                    return true;
+                }
+                temp = temp->nextNode;
+            }
+            return false; // element not found
+        }
+void print(node* &nodeInput) {
+            if (nodeInput == nullptr) {
+                std::cout << "empty list" << std::endl;
+                return;
+            }
+            node* temp = nodeInput;
+            while (temp != nullptr) {
+                std::cout << temp->n << " -> ";
+                temp = temp->nextNode;
+            } 
+            std::cout << "nullptr" << std::endl;
+        }
+bool isALoop(node* &nodeInput) {
+    node* turtle = nodeInput;
+    // 10 -> 20 -> 30 -> 40 -> 20
+    // turtle = 10, hare = 20;
+    node* hare = nodeInput;
+    while (hare != nullptr && hare->nextNode != nullptr) {
+        hare = hare->nextNode->nextNode; // goes +2
+        // hare = 40
+        turtle = turtle->nextNode; // goes +1
+        if (hare == turtle) { 
+            return true;
+        }
+        // turtle = 20
+        // second loop, hare = 30
+        // turtle = 30 ITS A LOOP
+    }
+    return false;
+}
+
+int main() {
+    node* list = nullptr;
+    list = insertLast(list, 10);
+    list = insertLast(list, 20);
+    list = insertLast(list, 30);
+    list = insertLast(list, 40);
+    std::cout << (isALoop(list) ? "the list enters in a loop" : "the list doesnt enters in a loop") << std::endl;
+    if (!isALoop(list)) {
+        print(list);
+    }
+    node* temp = list;
+    while (temp->nextNode != nullptr) {
+        temp = temp->nextNode;
+    }
+    temp->nextNode = list->nextNode;
+    std::cout << (isALoop(list) ? "the list enters in a loop" : "the list doesnt enters in a loop") << std::endl;
+    if (!isALoop(list)) {
+        print(list);
+    }
+    delete list;
+}
--- practica_cpp_con_copilot/18d_ejercicio_merge_listas.cpp
+++ practica_cpp_con_copilot/18d_ejercicio_merge_listas.cpp
@@ -14,3 +14,83 @@
  */
 
 // Tu código aquí
+#include <iostream>
+#include <vector>
+#include <algorithm>
+struct node {
+    int n;
+    node* nextNode;
+    node(int nW) : n(nW), nextNode(nullptr){};
+    ~node() {
+        std::cout << "node destroyed" << std::endl;
+    }
+};
+
+node* insertLast(node* nodeInput, int n) {
+            node* newNode = new node(n);
+            if (nodeInput == nullptr) {
+                nodeInput = newNode;
+                return nodeInput;
+            }
+            node* temp = nodeInput; // cause im using pointers; temp and nodeinput has the same pointer in memory
+            while (temp->nextNode != nullptr) {
+                temp = temp->nextNode; // not using nodeInput, cause it will lost the first element pointer
+            }
+            temp->nextNode = newNode;
+            return nodeInput;
+        }
+void print(node* &nodeInput) {
+            if (nodeInput == nullptr) {
+                std::cout << "empty list" << std::endl;
+                return;
+            }
+            node* temp = nodeInput;
+            while (temp != nullptr) {
+                std::cout << temp->n << " -> ";
+                temp = temp->nextNode;
+            } 
+            std::cout << "nullptr" << std::endl;
+        }
+node* mergeList(node* nodeInput1, node* nodeInput2) {
+    node* list1 = nodeInput1; // 10 -> 30 -> 50 -> nullptr
+    node* list2 = nodeInput2; // 20 -> 40 -> 60 -> nullptr
+    node* sortList = nullptr;
+    if (list1 != nullptr && (list2 == nullptr || list1->n <= list2->n)) {
+        sortList = list1;
+        list1 = list1->nextNode;
+    } else if (list2 != nullptr) {
+        sortList = list2;
+        list2 = list2->nextNode;
+    } // creates a unordered list, only to work with the size
+    node* temp = sortList;
+    while (list1 != nullptr && list2 != nullptr) {
+        if (list1->n <= list2->n) { // list1 is bigger
+            temp->nextNode = list1;
+            list1 = list1->nextNode;
+        } else { // list2 is bigger
+            temp->nextNode = list2;
+            list2 = list2->nextNode;
+        }
+        temp = temp->nextNode;
+    }
+    if (list1 != nullptr) {
+        temp->nextNode = list1;
+    } else if (list2 != nullptr) {
+        temp->nextNode = list2;
+    }
+    return sortList;
+}
+int main() {
+    node* list1 = nullptr;
+    node* list2 = nullptr;
+    node* mergeLists = nullptr;
+    list1 = insertLast(list1, 10);
+    list2 = insertLast(list2, 20);
+    list1 = insertLast(list1, 30);
+    list2 = insertLast(list2, 40);
+    list1 = insertLast(list1, 50);
+    list2 = insertLast(list2, 60);
+    print(list1); print(list2);
+    mergeLists = mergeList(list1, list2);
+    print(mergeLists);
+}
